60d5fc9c630da572082b4beeef4587b5c1e6baac
uma <abc@xyz.com>
2022-08-20 00:31:36 -0400

    new file

---
+++ server-script1.sh
@@ -0,0 +1,14 @@
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install git -y
+sudo yum install maven -y
+if [ -d "addressbook" ]
+then 
+    echo "repo is cloned and exits"
+    cd /home/ec2-user/addressbook
+    git pull origin b2
+else
+    echo "repo is not there"
+    git clone https://github.com/umajonna9/addressbook.git
+    cd /home/ec2-user/addressbook
+fi
+mvn package
