a5fb1ba14c2b06a04b3f09f85c432cd086d5fb10
Sandeep Kandukuri <sandeep.kandukuri@prutech.local>
2024-01-10 00:21:25 +0530

    hgjg

--- server-script.sh
+++ server-script.sh
@@ -5,7 +5,7 @@ if [ -d "addressbook" ]
 then
   echo "repo is cloned and exists"
   cd /home/ec2-user/addressbook
-  git pull origin devops-cicd
+  git pull origin docker-cicd
 else
   echo "repo is not there"
   git clone https://github.com/sandeep-kandukuri/addressbook.git
